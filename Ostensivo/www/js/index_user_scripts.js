93c7a66f8e578a1dea9163fb5418ad3b563fb680
Ana Naiane <ananaiane.sf@gmail.com>
2016-03-24 16:11:19 -0300

    atualizacao

---
+++ Ostensivo/www/js/index_user_scripts.js
@@ -0,0 +1,13 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+ }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
